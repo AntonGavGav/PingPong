72bb7dbe8e76cd3ab93fe5892e5a8739feec0547
EventfulLynx915 <gavrylenko.anton@gmail.com>
2023-01-14 16:51:46 +0100

    add screen size adaptation

---
+++ Assets/Scripts/OnStartBlockPlacing.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class OnStartBlockPlacing : MonoBehaviour
+{
+    [SerializeField] private Transform topWall, bottomWall, ball, player1, player2, goalBlock1, goalBlock2;
+
+    private void Start()
+    {
+        //getting usefull info for later
+        Camera cam = Camera.main;
+        float screenWidth = 1/(cam.WorldToViewportPoint(new Vector3(1,1,0)).x - .5f);
+        float screenHeight = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f);
+        
+        //setting up ball
+        ball.transform.localScale = new Vector3(screenWidth/48, screenWidth/48);
+        ball.position = new Vector2(0f, 0f);
+        //setting up walls
+        topWall.localScale = new Vector3(screenWidth, 1);
+        topWall.position = new Vector3(0, screenHeight/2 + topWall.localScale.y/2);
+        bottomWall.localScale = new Vector3(screenWidth, 1);
+        bottomWall.position = new Vector3(0, -screenHeight/2 - bottomWall.localScale.y/2);
+        //setting up goal blocks
+        goalBlock1.localScale = new Vector3(1, screenHeight);
+        goalBlock1.position = new Vector3(-screenWidth / 2 - goalBlock1.localScale.x / 2, 0);
+        goalBlock2.localScale = new Vector3(1, screenHeight);
+        goalBlock2.position = new Vector3(screenWidth / 2 + goalBlock2.localScale.x / 2, 0);
+        //setting up players 
+        player1.localScale = new Vector3(screenWidth / 72, screenHeight / 4f);
+        player1.position = new Vector3(-screenWidth / 2.2f, 0);
+        player2.localScale = new Vector3(screenWidth / 72, screenHeight / 4f);
+        player2.position = new Vector3(screenWidth / 2.2f, 0);
+    }
+}
